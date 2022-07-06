48eddaacb6d25ff010c77538b62c6348c0bd5c77
George Baroi <baroi.george@gmail.com>
2022-07-06 18:09:00 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+const dodger = document.getElementById("dodger")
+dodger.style.backgroundColor = "#FF69B4"
+
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
+  
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+       
+      dodger.style.left = `${left - 10}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+   
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight(){
+    const rightNumbers = dodger.style.left.replace("px","")
+    const right = parseInt(rightNumbers, 10)
+
+    if (right >= 0 && right < 360 ){
+        
+        dodger.style.left= `${right +10}px`
+    }
+  }
+  document.addEventListener("keydown", function(e){
+    if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+  }
+  )
